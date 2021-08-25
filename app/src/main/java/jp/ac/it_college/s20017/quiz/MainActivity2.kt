9b6f0357d516373b062960e979d889e678999b95
s20017 <s20017@std.it-college.ac.jp>
2021-08-25 15:21:19 +0900

    1

---
+++ app/src/main/java/jp/ac/it_college/s20017/quiz/MainActivity2.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.s20017.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
